d6675ebc9f4e32626ae3e61393db09e1f3105451
Nathan Frost <nathan.frost@gmail.co>
2011-11-09 20:12:40 -0800

    Create helpers for copyURL and pasteURL. Add to userPage bookmark template. Paste URL now domain.com/user/p/ Copy URL now domain.com/user/c/copythisstuff `

--- server/server.rb
+++ server/server.rb
@@ -26,6 +26,12 @@ class Controller < Sinatra::Base
         return false unless File.file? path
         File.open(path, 'r') { |file| file.read }
     end
+    def pasteURL userName
+        url = "#{APP_DOMAIN}/#{userName}/p/"
+    end
+    def copyURL userName
+        url = "#{APP_DOMAIN}/#{userName}/c/"
+    end
   end
 
   get '/' do
@@ -35,8 +41,8 @@ class Controller < Sinatra::Base
     redirect redirect_page name unless false 
   end
   
-  get %r{^/([a-zA-Z0-9-]+)/add$} do |name|
-    haml :userPage, :locals => { :userName => name } 
+  get %r{^/add/([a-zA-Z0-9-]+)$} do |name|
+    haml :userPage, :locals => { :userName => name, :pasteURL => pasteURL name, :copyURL => copyURL name } 
   end
   get '/*/p/*' do |name, url|
     save_page name,url
